8bf8c0172989f6c5024047965a67235299e782e5
Fabiola Li Wu <fabiolalw@users.noreply.github.com>
2025-03-10 18:51:52 -0400

    MongoDB loading data in batches

--- MongoDB.py
+++ MongoDB.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import pandas as pd
 
 class MongoDB:
-    def __init__(self, uri="mongodb://localhost:27017/", db="hetio_database"):
+    def __init__(self, uri="mongodb+srv://fabiolaliwu:fabiolaliwu@cluster0.nps6h.mongodb.net/", db="hetio_database"):
         self.client = MongoClient(uri)
         self.db = self.client[db]
         self.nodes_collection = self.db['nodes']
@@ -11,12 +11,38 @@ class MongoDB:
     def cleanDatabase(self):
         self.db.nodes.drop()
         self.db.edges.drop()
-        print("Database cleaned in MongoDB")
-  
+        print("database cleaned")
+
     def loadNodes(self, node_file='nodes.tsv'):
         nodes_df = pd.read_csv(node_file, sep='\t')
         nodes_data = nodes_df.to_dict(orient='records')
-        print(f"{len(nodes_data)} nodes loaded into MongoDB") # testing purposes
+        compoundList = []
+        diseaseList = []
+        anatomyList = []
+        geneList = []
+        for node in nodes_data:
+            if node['kind'] == 'Compound':
+                compoundList.append(node)
+            elif node['kind'] == 'Disease':
+                diseaseList.append(node)
+            elif node['kind'] == 'Anatomy':
+                anatomyList.append(node)
+            elif node['kind'] == 'Gene':
+                geneList.append(node)
+        self.nodes_collection.insert_many(compoundList)
+        self.nodes_collection.insert_many(diseaseList)
+        self.nodes_collection.insert_many(anatomyList)
+        self.nodes_collection.insert_many(geneList)
 
-    
+        #testing purposes
+        print(f"{len(compoundList)} inserted into the DB for compound")
+        print(f"{len(diseaseList)} inserted into the DB for disease")
+        print(f"{len(anatomyList)} inserted into the DB for anatomy")
+        print(f"{len(geneList)} inserted into the DB for gene")
 
+        # if nodes_data:
+        #     self.nodes_collection.insert_many(nodes_data)
+        #     # testing purposes
+        #     print(f"{len(nodes_data)} nodes loaded into MongoDB")
+        # else:
+        #     print("No data to insert.")
--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from Neo4J import Neo4J
 
 if __name__ == "__main__":
     testingMongoDB = MongoDB()
-    testingNeo4J = Neo4J()
     print("Loading nodes into MongoDB")
     testingMongoDB.cleanDatabase()  
     testingMongoDB.loadNodes()
     mongo_client = testingMongoDB.client
     mongo_db = mongo_client['hetio_database']
     nodes_collection = mongo_db['nodes'] 
-    testingNeo4J.cleanDatabase()
+
+    # testingNeo4J = Neo4J()
+    # testingNeo4J.cleanDatabase()
     
    
 
