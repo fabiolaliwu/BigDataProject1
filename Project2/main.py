17b9e1fed0d60714771258c45aed9676aa4cf993
Fabiola Li Wu <fabiolaliwu@users.noreply.github.com>
2025-04-13 00:56:20 -0400

    finished project

--- Project2/main.py
+++ Project2/main.py
@@ -3,9 +3,17 @@ from queries import Queries
 
 #create an instamce for the class and call the query1 function
 q = Queries()
-q.query(1)
-q.query2(20)
-q.query(3)
+
+while True:
+    queryNumber = int(input("\nQuery 1: Top 5 compounds with most genes associated, and their associated diseases\nQuery 2: Query 2: Top 5 diseases with drugs associated\nQuery 3: Names of top 5 compounds with most genes associate.\nPress 0: EXIT\nEnter the query number: "))
+    if queryNumber in [1, 3]:
+        q.query(queryNumber)
+    elif queryNumber == 2:
+        n = int(input("Enter the number of drugs: "))
+        q.query2(n)
+    else:
+        print("Invalid query number. Please try again.")
+        break
 
 
     
