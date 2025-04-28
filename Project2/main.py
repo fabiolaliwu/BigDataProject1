fae20ac8f684bbf5d8de27031dc818c41d60e191
Fabiola Li Wu <fabiolaliwu@users.noreply.github.com>
2025-04-28 10:47:02 -0400

    fixed little mispelling

--- Project2/main.py
+++ Project2/main.py
@@ -5,7 +5,7 @@ from queries import Queries
 q = Queries()
 
 while True:
-    queryNumber = int(input("\nQuery 1: Top 5 compounds with most genes associated, and their associated diseases\nQuery 2: Query 2: Top 5 diseases with drugs associated\nQuery 3: Names of top 5 compounds with most genes associate.\nPress 0: EXIT\nEnter the query number: "))
+    queryNumber = int(input("\nQuery 1: Top 5 compounds with most genes associated, and their associated diseases\nQuery 2: Top 5 diseases with drugs associated\nQuery 3: Names of top 5 compounds with most genes associate.\nPress 0: EXIT\nEnter the query number: "))
     if queryNumber in [1, 3]:
         q.query(queryNumber)
     elif queryNumber == 2:
